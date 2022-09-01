c864471d8c36d2a32b2eaa54f26a2f460235d7c1
Austin Williams <awilliams@pangotechnology.com>
2022-09-01 15:36:49 -0800

    cards now display rating and matching color outline

--- SynergySearch/Pages/Gun.cs
+++ SynergySearch/Pages/Gun.cs
@@ -1,6 +1,39 @@
 ﻿public class Gun
 {
-    public string Name { get; set; }
-    public string Link { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Link { get; set; }
+    public string? Quote { get; set; }
+    public string? Description { get; set; }
+    public string? Rating { get; set; }
+
+    public string? RatingLink
+    {
+        get
+        {
+            return $"rating-icons/{this.Rating}.png";
+        }
+    }
+
+    public string BorderColor
+    {
+        get
+        {
+            switch (this.Rating) { 
+
+                case "S":
+                    return "orange";
+                case "A":
+                    return "red";
+                case "B":
+                    return "green";
+                case "C":
+                    return "DarkTurquoise";
+                case "D":
+                    return "SaddleBrown";
+                default:
+                    return "grey";
+   
+            }
+        }
+    }
 }
