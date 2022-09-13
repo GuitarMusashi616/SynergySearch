0172f47a1fa50101698acd2a98d6da6759fea603
Austin Williams <awilliams@pangotechnology.com>
2022-09-13 13:54:12 -0800

    added synergies page, made search component require route argument

---
+++ SynergySearch/Pages/Synergy.cs
@@ -0,0 +1,75 @@
+﻿public class Synergy
+{
+    private string? _name;
+    private string? _link;
+    private string? _rating;
+    private string? _ratingString;
+    private string? _borderColor;
+    public string? Name {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            _name = value;
+            _link = $"icons/{_name!.Replace(' ', '_')}.png";
+        } 
+    }
+    public string? Link
+    {
+        get
+        {
+            return _link;
+        }
+    }
+    public string? Quote { get; set; }
+    public string? Description { get; set; }
+    public string? Rating {
+        get
+        {
+            return _rating;
+        }
+        set
+        {
+            _rating = value;
+            _ratingString = $"rating-icons/{_rating}.png";
+            _borderColor = MatchQualityToColor(_rating!);
+        }
+    }
+    public string? RatingLink
+    {
+        get
+        {
+            return _ratingString;
+        }
+    }
+
+    public string? BorderColor
+    {
+        get
+        {
+            return _borderColor;
+        }
+    }
+
+    public static string MatchQualityToColor(string rating)
+    {
+        switch (rating) { 
+
+            case "S":
+                return "orange";
+            case "A":
+                return "red";
+            case "B":
+                return "green";
+            case "C":
+                return "DarkTurquoise";
+            case "D":
+                return "SaddleBrown";
+            default:
+                return "grey";
+
+        }
+    }
+}
